97607b9dcd8a8c5f86dc5772952974dc50185d45
deerak-isaack <oderaisaack@gmail.com>
2024-06-11 11:15:49 +0300

    edit sql model to rename the columns

--- TCL_Taxi_Analysis/models/example/table_rename.sql
+++ TCL_Taxi_Analysis/models/example/table_rename.sql
@@ -13,20 +13,20 @@ select
   '3' as dropoff_daytime,
   '4' as passenger_count,
   '5' as Trip_distance,
-  '6' as PULocationID,
-  '7' as DOLocationID,
-  '8' as RateCodeID,
-  '9' as store_and_forawd,
+  '6' as RateCodeID,
+  '7' as store_and_forward,
+  '8' as PULocationID,
+  '9' as DOLocationID,
   '10' as payment_type,
   '11' as Fare_amount,
   '12' as Extra,
   '13' as MTA_tax,
-  '14' as improovement_surchage,
-  '15' as Trip_amount,
-  '16' as Tolls_amount,
-  '17' as Tolls_amount,
-  '18' as Total_amount,
-  '19' as congestion_surcharge
+  '14' as trip_amount,
+  '15' as Tolls_amount,
+  '16' as improovement_surchage,
+  '17' as Total_amount,
+  '18' as congestion_surchage,
+  '19' as airport_fee 
 from {{ ref('green_taxi') }}
 
 
